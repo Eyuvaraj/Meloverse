a700e6e3c04e453bda4022a4449170b4dda9eec6
Eyuvaraj <21f2000664@ds.study.iitm.ac.in>
2023-10-26 22:01:28 +0530

    admin-dashboard: user management...

--- application/app_controllers/admin.py
+++ application/app_controllers/admin.py
@@ -1,7 +1,6 @@
 from flask import redirect, url_for, render_template, request, Flask, flash, Blueprint
-from flask import current_app as app
 from ..database import db
-from ..models import Users
+from ..models import *
 from flask_login import UserMixin, LoginManager, login_user, login_required, logout_user, current_user
 from application.app_controllers import auth
 
@@ -15,7 +14,18 @@ def admin_dashboard(user):
 @admin.route("/admin/<user>/user_manager", methods=["GET", "POST"])
 @login_required
 def user_management(user):
-    return render_template("admin/user_mg.html")
+    if request.method=="POST":
+        user_id = request.form.get("user_id")
+        grant = request.form.get("grant")
+        revoke = request.form.get("revoke")
+        user=Users.query.get(user_id)
+        if grant=="yes":
+            db.session.query(Users).filter_by(id=user_id).update({"role":"admin"})
+        elif revoke=="yes":
+            db.session.query(Users).filter_by(id=user_id).update({"role":"user"})
+        db.session.commit()
+    users_=db.session.query(Users, Creator).outerjoin(Creator, Users.id == Creator.creator_id).all()
+    return render_template("admin/user_mg.html", users=users_)
 
 @admin.route("/admin/<user>/songs_manager", methods=["GET", "POST"])
 @login_required
